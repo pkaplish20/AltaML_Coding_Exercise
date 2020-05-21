5bad79858cdc348e95c8b48fae5fda8c3c4a8f14
pkaplish20 <58612059+pkaplish20@users.noreply.github.com>
2020-05-20 23:58:03 -0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -6,16 +6,18 @@ from coding_exercise import get_coordinates
 app = Flask(__name__)
 
 
+# render index.html from temlates folder
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+# post the text file to api and get the coordinates of path traveled and orientation
 @app.route('/uploadFile', methods=['POST'])
 def extractInfo():
-    files = request.files.getlist('file')
+    files = request.files.getlist('file')  # get the file from frontend
     files[0].seek(0)
-    data = files[0].read().decode('UTF-8')
+    data = files[0].read().decode('UTF-8')  # read the file
     coord, orientation = get_coordinates(data)
     return jsonify({'coordinates_list': coord, 'last_orientation': orientation})
 
